e5609fbcbe12bb239b6589619e3a5338e10c171c
zhangpanzhao <zhangpanzhao@okay.cn>
2020-03-11 23:20:04 +0800

    [update] 链表中添加元素

--- 04_LinkedList/src/main/java/LinkedList.java
+++ 04_LinkedList/src/main/java/LinkedList.java
@@ -1,6 +1,64 @@
 
 public class LinkedList<E> {
 
+    private Node head;
+    private int size;
+
+    public LinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * 在链表头添加元素
+     * @param e 添加的元素
+     */
+    public void addFirst(E e) {
+        Node node = new Node(e);
+        node.next = head;
+        head = node;
+
+        // head = new Node(e, head); // 优雅的写法
+        size++;
+    }
+
+    /**
+     * 在链表的 index(0-based) 位置添加新的元素e
+     * @param index 添加的位置
+     * @param e 添加的元素
+     *
+     *          不是一个常用的操作，练习用
+     */
+    public void add(int index, E e) {
+        if (index == 0) {
+            addFirst(e);
+        } else {
+            Node prev = head;
+            for (int i = 0; i < index - 1; i++) {
+                prev = prev.next;
+            }
+
+            Node node = new Node(e);
+            node.next = prev.next;
+            prev.next = node;
+
+            prev.next = new Node(e, prev.next); // 优雅的写法
+        }
+        size++;
+    }
+
+    public void addLast(E e) {
+        add(size, e);
+    }
+
     private class Node {
         public E e;
         public Node next;
