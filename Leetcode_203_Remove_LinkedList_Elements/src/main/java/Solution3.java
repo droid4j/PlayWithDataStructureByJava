13260143d5bca24cfa7d1913fb8308bc8ea1c7df
droid4j <android4j@163.com>
2020-04-06 07:44:55 +0800

    [update] 简化

--- Leetcode_203_Remove_LinkedList_Elements/src/main/java/Solution3.java
+++ Leetcode_203_Remove_LinkedList_Elements/src/main/java/Solution3.java
@@ -4,12 +4,7 @@ public class Solution3 {
     public ListNode removeElements(ListNode head, int val) {
         if (head == null) return null;
 
-        ListNode res = removeElements(head.next, val);
-        if (head.val == val) {
-            return res;
-        } else {
-            head.next = res;
-            return head;
-        }
+        head.next = removeElements(head.next, val);
+        return head.val == val ? head.next : head;
     }
 }
