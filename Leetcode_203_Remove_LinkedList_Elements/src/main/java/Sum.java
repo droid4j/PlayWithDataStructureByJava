f1de6fd58182838a43307ca584614e622a2a2fe7
droid4j <android4j@163.com>
2020-04-05 21:42:27 +0800

    [update] 使用递归求和

---
+++ Leetcode_203_Remove_LinkedList_Elements/src/main/java/Sum.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class Sum {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+        System.out.println(Arrays.toString(arr));
+        System.out.println(sum(arr));
+    }
+
+    public static int sum(int[] arr) {
+        return sum(arr, 0);
+    }
+
+    private static int sum(int[] arr, int l) {
+        if (l == arr.length) {
+            return 0;
+        }
+        return arr[l] + sum(arr, l + 1);
+    }
+}
