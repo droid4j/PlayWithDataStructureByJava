39cfe784ef84c4429cf2cb1443804cfc8892c933
droid4j <android4j@163.com>
2020-04-05 21:36:05 +0800

    [update] 本地模拟 leetcode，进行测试

--- Leetcode_203_Remove_LinkedList_Elements/src/main/java/ListNode.java
+++ Leetcode_203_Remove_LinkedList_Elements/src/main/java/ListNode.java
@@ -6,4 +6,31 @@ public class ListNode {
     public ListNode(int val) {
         this.val = val;
     }
+
+    // 链表节点的构造函数
+    // 使用 arr 为参数，创建一个链表，当前的 ListNode 为链表的头结点
+    public ListNode(int[] arr) {
+        if (arr == null || arr.length == 0)
+            throw new IllegalArgumentException("arr can not be empty");
+
+        this.val = arr[0];
+        ListNode cur = this;
+        for (int i = 1; i < arr.length; i++) {
+            cur.next = new ListNode(arr[i]);
+            cur = cur.next;
+        }
+    }
+
+    // 以当前结点为头结点的链表信息字符串打印
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        ListNode cur = this;
+        while (cur != null) {
+            res.append(cur.val + "->");
+            cur = cur.next;
+        }
+        res.append("NULL");
+        return res.toString();
+    }
 }
